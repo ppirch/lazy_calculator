dcd7b4e834d6f39cabb3ab23b534d49ca64ce5e8
ppirch <pakin.pirch@gmail.com>
2023-06-09 23:10:31 +0700

    perf(factorial): use loop instead of recursive

--- lazy_calculator/factorial.py
+++ lazy_calculator/factorial.py
@@ -14,5 +14,9 @@ def factorial(n: int) -> int:
 
     if n == 0:
         return 1
-    else:
-        return n * factorial(n-1)
+
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+
+    return factorial
