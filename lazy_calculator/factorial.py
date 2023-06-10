c3bee09b46550ba469aea75c35e24bba511d957b
ppirch <pakin.pirch@gmail.com>
2023-06-10 17:18:58 +0700

    refactor: fix redefining

--- lazy_calculator/factorial.py
+++ lazy_calculator/factorial.py
@@ -15,8 +15,8 @@ def factorial(n: int) -> int:
     if n == 0:
         return 1
 
-    factorial = 1
+    fact = 1
     for i in range(1, n+1):
-        factorial *= i
+        fact *= i
 
-    return factorial
+    return fact
